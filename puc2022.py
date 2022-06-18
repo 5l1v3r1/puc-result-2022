8e5a22c5c248e08fcee0ce1c5e6a290abc7a751c
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-18 13:04:33 +0530

    Update puc2022.py

--- puc2022.py
+++ puc2022.py
@@ -8,7 +8,7 @@ driver.get("https://karresults.nic.in/slpuind.asp")
 reg= driver.find_element_by_name("reg").send_keys("000000") #enter required puc reg no 
 
 dropdown = Select(driver.find_element_by_id("ddlsub"))
-dropdown.select_by_value("S")
+dropdown.select_by_value("S")  #for arts select "A" for commerce select "C"
 
 button=driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/form/button').click()
 
